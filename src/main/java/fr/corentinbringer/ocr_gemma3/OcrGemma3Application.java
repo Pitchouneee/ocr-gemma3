fd1566edf1b3ceb04e0cde2db6b8e93b8a333109
Corentin Bringer <corentinbringer@ik.me>
2025-04-05 10:50:10 +0200

    first commit

---
+++ src/main/java/fr/corentinbringer/ocr_gemma3/OcrGemma3Application.java
@@ -0,0 +1,13 @@
+package fr.corentinbringer.ocr_gemma3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrGemma3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrGemma3Application.class, args);
+	}
+
+}
